64e4623b86bf23e0b1d278bf36bd8852fe26516e
vns_lzrg <veneesejulialazaraga@gmail.com>
2026-03-27 14:06:20 +0800

    Done playing and updating the 02-vanilla-js-types.js

--- 02-vanilla-js-types.js
+++ 02-vanilla-js-types.js
@@ -1,3 +1,22 @@
-let userName1 = "Elmer";
+let name = "Julia";
+let age = 23;
+let course1 = "BSIS";
+let isEnrolled1 = true;
+let score1 = 1.0;
+let tuitionFee1 = null;
 
-console.log(typeof userName1)
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof course1)
+console.log(typeof isEnrolled1)
+console.log(typeof score1)
+console.log(typeof tuitionFee1)
+
+
+console.log (1 == "1")
+console.log (1 === "1")
+
+console.log (1 + "1")
+console.log (1 + 1)
+
+
