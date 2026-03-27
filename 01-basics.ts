aec6efe0a93bd2ad802d97b4605263de124bda14
vns_lzrg <veneesejulialazaraga@gmail.com>
2026-03-27 13:49:22 +0800

    Done playing and updating the 01-basic.ts file

--- 01-basics.ts
+++ 01-basics.ts
@@ -1,20 +1,31 @@
 // console.log("Hello World!")
 
-let userName: string = "Elmer";
-let userAge = 31;
+let userName: string = "Veneese";
+let userAge: number = 20;
+let course : string = "BSIS";
+let isEnrolled: boolean = true;
 
 // ...
 
-userAge = "31"
+userAge = 20
 
 console.log(`Username - ${userName}`)
 console.log(`Age - ${userAge}`)
+console.log(`Course - ${course}`)
+console.log(`Enrolled - ${isEnrolled}`)
 
-function add(a: number, b = 5) {
+function add(a: number, b: number = 5) {
     return a + b
 }
 
-add(10)
+console.log(add(20))
 // add('10')
-add(10, 6)
+console.log(add(4, 4))
 // add(10, '6')
+
+function greetStudent(name: string, age: number): string {
+    return `Hi ${name}, you are ${age} years old!`
+}
+
+console.log(greetStudent(userName, userAge))
+console.log(greetStudent("userName", userAge))
