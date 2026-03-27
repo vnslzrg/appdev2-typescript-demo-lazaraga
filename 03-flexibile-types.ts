bb00b29cce72ab7e431009cc194af67bbe48dcb0
vns_lzrg <veneesejulialazaraga@gmail.com>
2026-03-27 14:19:48 +0800

    Done playing and updating the 03-flexibile-types.ts file

--- 03-flexibile-types.ts
+++ 03-flexibile-types.ts
@@ -1,10 +1,24 @@
-let age: any = 31
+let age: any = 21
+console.log(`Age - ${age} | type: ${typeof age}`)
+
+age = "21"
+console.log(`Age - ${age} | type: ${typeof age}`)
+
+age = true
+console.log(`Age - ${age} | type: ${typeof age}`)
+
+age ={}
+console.log(`Age - ${age} | type: ${typeof age}`)
+
+age = []
+console.log(`Age - ${age} | type: ${typeof age}`)
 
 // ...
 
-age = '31'
-age = true
-age = {}
-age = []
+let score: any = "100"
+score = 100
+score = "one hundred"
+console.log(`Score - ${score} | type: ${typeof score}`)
 
-console.log(typeof age)
+let greeting: string = "Hello World!"
+console.log(`Greeting - ${greeting} | type: ${typeof greeting}`)
