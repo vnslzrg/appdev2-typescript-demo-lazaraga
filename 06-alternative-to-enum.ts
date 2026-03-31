c2cb4d2d1759004d7187f69c089b9691ee4c4276
vns_lzrg <veneesejulialazaraga@gmail.com>
2026-03-31 13:10:24 +0800

    Done playing and updating 06-alternative-to-enum.ts file

--- 06-alternative-to-enum.ts
+++ 06-alternative-to-enum.ts
@@ -1,11 +1,57 @@
 (() => {
     // Literal Types (Alternative to Enum)
-    let userRole: 'admin' | 'guess' | 'teacher' | 'student' = 'admin'
+    let userRole: 'IT' | 'UI/UX' | 'PM' | 'SQA' = 'UI/UX'
 
-    userRole = 'guess'
+    console.log(`User Role: ${userRole}`)
     // anotherUserRole = 'superadmin'
+    userRole = 'PM'
+    console.log(`User Role: ${userRole}`)
 
-    function access(role: 'admin' | 'guess' | 'teacher' | 'student') {
-        // ...
+    function getAccess(role: 'admin' | 'guest' | 'teacher' | 'student') {
+        if (role === 'Admin') {
+            return 'Full Access - manage everything'
+        }
+        if (role === 'teacher') {
+            return 'Partial Access - manage students and courses'
+        }
+        if (role === 'student') {
+            return 'Limited Access - view courses and submit assignments'
+        }
+        if (role === 'guest') {
+            return 'Guest Access - view public content only'
+        }
+        return 'Invalid role'
     }
-})
+    console.log(getAccess('admin'))
+    console.log(getAccess('teacher'))
+    console.log(getAccess('student'))
+    console.log(getAccess('member'))
+
+    //using enum
+    enum UserRole {
+        IT = 'IT',
+        UI_UX = 'UI/UX',
+        PM = 'PM',
+        SQA = 'SQA'
+    }
+
+    //using literal types
+    type UserRoleType = 'IT' | 'UI/UX' | 'PM' | 'SQA'
+
+    let role1: UserRole = UserRole.PM
+    let role2: UserRoleType = 'IT'
+
+    console.log(`Role1: ${role1}`)
+    console.log(`Role2: ${role2}`)
+
+    //using alias to reuse the same literal type
+    type Status = 'present' | 'absent' | 'excuse'
+    type Subjects = 'Math' | 'Science' | 'History'
+
+    let attendanceStatus: Status = 'Present'
+    let subject: Subjects = 'English'
+    
+    console.log('Attendance Status:', attendanceStatus)
+    console.log('Subject:', subject)
+    console.log (`Status: ${attendanceStatus}, Subject: ${subject}`)
+})()
