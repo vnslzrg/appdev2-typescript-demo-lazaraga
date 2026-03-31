6e2655a8849c7fb99b6f110e50cdebab01d3e8e5
vns_lzrg <veneesejulialazaraga@gmail.com>
2026-03-31 14:24:43 +0800

    Done playing and updating the 09-special-types.ts file

--- 09-special-types.ts
+++ 09-special-types.ts
@@ -1,25 +1,66 @@
 // Special Type - null
-let a: null;
+// let a: null;
 
-a = null
-// a = "Hi"
+// a = null
+// // a = "Hi"
 
-let b: null | string
+// let b: null | string
 
-b = null
-b = "Hi"
-// b = 10
+// b = null
+// b = "Hi"
+// // b = 10
+
+type LoggedInUser = {
+    name: string | null;
+    email: string | null;
+}
+
+let currentUser: LoggedInUser | null = null
+currentUser = {
+    name: null,
+    email: null
+}
+console.log(`\n--Login--`)
+console.log(`Before login: ${currentUser??'No user logged in'}`)
+
+currentUser = {
+    name: "Julia",
+    email: "julia@gmail.com"
+}
+
+console.log(`\n--Login--`)
+console.log(`After login: ${currentUser?.name}`)
+console.log(`Email: ${currentUser?.email}`)
+
 
 // Special Type - undefined
-let c: undefined;
+// let c: undefined;
 
-c = undefined
-// c = null
-// c = "Hi"
+// c = undefined
+// // c = null
+// // c = "Hi"
 
-let d: undefined | string
+// let d: undefined | string
 
-d = undefined
-d = "Hi"
+// d = undefined
+// d = "Hi"
 // d = null
 // d = 10
+type UserProfile = {
+    firstName: string;
+    lastName: string;
+    email: string | undefined;
+    phone: string | undefined;
+}
+
+const userProfile: UserProfile = {
+    firstName: "Veneese",
+    lastName: "Lee",
+    email: undefined,
+    phone: undefined
+}
+
+console.log(`\n--User Profile--`)
+console.log(`Name: ${userProfile.firstName} ${userProfile.lastName}`)
+console.log(`Email: ${userProfile.email ?? 'No email provided'}`)
+console.log(`Phone: ${userProfile.phone ?? 'No phone provided'}`)
